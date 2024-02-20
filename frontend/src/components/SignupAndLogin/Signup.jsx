1fffc39a13ac8af6f12ff415cfc5cbc0036adb5c
Jimmy Clairy <jimmyclairy@gmail.com>
2024-02-20 18:57:20 +0100

    Improve Signup

--- frontend/src/components/SignupAndLogin/Signup.jsx
+++ frontend/src/components/SignupAndLogin/Signup.jsx
@@ -1,64 +1,56 @@
-import React, { useState } from 'react'
-import { useNavigate/*, Link*/ } from 'react-router-dom'
-import Cookies from 'js-cookie'
-import fetchData from '../../Fetch/fetchData.js'
-
-import "./S-L.css"
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Cookies from 'js-cookie';
+import fetchData from '../../Fetch/fetchData.js';
+import "./S-L.css";
 
 export default function Signup({ signupOrLogin, setSignupOrLogin }) {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+  const [user, setUser] = useState({ pseudo: '', email: '', password: '' });
+  const [txtError, setTxtError] = useState('');
 
-  const [pseudo, setPseudo] = useState('')
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-
-  const infoUser = {
-    pseudo: pseudo,
-    email: email,
-    password: password,
-  }
-
-  const [txtError, setTxtError] = useState('')
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setUser({ ...user, [name]: value });
+    setTxtError('');
+  };
 
   const signup = async (e) => {
     e.preventDefault();
     try {
-
       const url = 'http://localhost:7000/api/user/signup';
       const options = {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(infoUser)
+        body: JSON.stringify(user)
       };
-
       await fetchData(url, options);
-
-      afterSignup(infoUser)
+      await loginAfterSignup(user);
     } catch (error) {
-      console.log(error);
+      console.error(error);
+      setTxtError(`Une erreur s'est produite lors de la connexion. Veuillez réessayer.`);
     }
   };
 
-  const afterSignup = async (infoUser) => {
+  const loginAfterSignup = async (user) => {
     try {
-
       const url = 'http://localhost:7000/api/user/login';
       const options = {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(infoUser)
+        body: JSON.stringify(user)
       };
 
       const data = await fetchData(url, options);
+      localStorage.setItem('userId', data.userId);
+      Cookies.set('token', data.token, { expires: 1, secure: true });
+      navigate('/home');
 
-      localStorage.setItem('userId', data.userId)
-      Cookies.set('token', data.token, { expires: 1, secure: true })
-      navigate('/home')
     } catch (error) {
-      console.log(error);
-      setTxtError('Peudo déja utilisé')
+      console.error(error);
+      setTxtError("Une erreur s'est produite lors de la connexion. Veuillez réessayer.");
     }
-  }
+  };
 
   return (
     <div className='S-L'>
@@ -71,30 +63,22 @@ export default function Signup({ signupOrLogin, setSignupOrLogin }) {
             minLength={5}
             maxLength={30}
             placeholder="Pseudo"
-            value={pseudo}
-            onChange={(e) => {
-              setPseudo(e.target.value)
-              setTxtError('')
-            }}
+            value={user.pseudo}
+            onChange={handleInputChange}
             required
           />
         </label>
-
         <label>
           <input
             type="email"
             name="email"
-            placeholder="Adress e-mail"
+            placeholder="Adresse e-mail"
             autoComplete="email"
-            value={email}
-            onChange={(e) => {
-              setEmail(e.target.value)
-              setTxtError('')
-            }}
+            value={user.email}
+            onChange={handleInputChange}
             required
           />
         </label>
-
         <label>
           <input
             type="password"
@@ -102,20 +86,16 @@ export default function Signup({ signupOrLogin, setSignupOrLogin }) {
             placeholder="Mot de passe"
             name="password"
             autoComplete="current-password"
-            value={password}
-            onChange={(e) => {
-              setPassword(e.target.value)
-              setTxtError('')
-            }}
+            value={user.password}
+            onChange={handleInputChange}
             required
           />
         </label>
-
         <button className='S-L__btn'>Créer un nouveau compte</button>
       </form>
-      <p className='TxtErr'>{txtError}</p>
+      {txtError && <p className='TxtErr'>{txtError}</p>}
       <div className='Separator'></div>
       <button className='S-L__btn' onClick={() => setSignupOrLogin(!signupOrLogin)}>Se connecter</button>
     </div>
-  )
+  );
 }
